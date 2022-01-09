a689ae14032c7b979667237af0c22da891ed311e
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-09 15:19:15 +0000

    commit correction

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const result = document.querySelector(".result");
 btn.addEventListener("click", palindrome);
 
 // odd number of letters first two and last two the same one left standalone the letter that's the odd one out will always be in the middle
-
+// redivider 9/2 = 4.5 4
 function palindrome() {
   const word = document.querySelector(".input-text").value;
   let length = word.length;
+  if (!word) return;
   //get first letters
-  //if odd will round down and get a certain number of letters, e.g 7/2 = 3.5 rounded down will get 3
+  //if odd will round down and get a certain number of letters, from word depending on length e.g 7/2 = 3.5 rounded down will get 3
   let start = word.substring(0, Math.floor(length / 2)).toLowerCase();
   let end = word.substring(length - Math.floor(length / 2)).toLowerCase(); //start from whichever index in string is result of expression
 
@@ -23,8 +24,4 @@ function palindrome() {
   } else {
     result.innerHTML = `${word.toUpperCase()} is not a palindrome`;
   }
-  //   alert(flip);
-  //   alert(start);
-  //   alert(end);
 }
-// console.log(5 % 2);
